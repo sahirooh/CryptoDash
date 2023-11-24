ec35c38dcf99c611a67378c8dde5b2962d2fc9d9
sahirooh <sahirooh@gmail.com>
2023-11-24 14:20:07 +0530

    added form

--- src/pages/Support/SupportPage.jsx
+++ src/pages/Support/SupportPage.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navigation from '../../components/Navigation';
 import Header from '../../components/Header';
+import Intro from './Components/Intro';
+import Form from './Components/Form';
 
 const Support = () => {
   return (
     <div className="flex">
       <Navigation />
       <div className="flex-grow">
-        <Header title={'Support'} />
+        <Header title={"Support"} />
+        <div className=" bg-[#EEEEF4] h-[calc(100vh-64px)]">
+          <div className="flex justify-center gap-6">
+            <Intro />
+            <Form />
+          </div>
+        </div>
       </div>
     </div>
   );
